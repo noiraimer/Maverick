eae6643ec4833b392568658842b484e2302644b5
noiraimer <liushu1187419589@live.com>
2020-02-16 11:47:34 +0800

    Update

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 
 # For Maverick
 site_prefix = "/"
-source_dir = "../src/"
-build_dir = "../dist/"
+source_dir = "./src/"
+build_dir = "./dist/"
 template = "Kepler"
 index_page_size = 10
 archives_page_size = 30
